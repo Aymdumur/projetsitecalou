06e0b6cfd13045039c7f0528032c806727f1744d
StarkillerV22 <75523544+StarkillerV22@users.noreply.github.com>
2021-04-06 11:35:28 +0200

    Test JS 3.4.6

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -16,10 +16,7 @@ var butG1 = $( "#button__G1" );
 var gundamAnimeTab = [
     [
         [
-            "<div> <h1>Mobile Suit Gundam</h1> <p>Année de sortie : 1979</p> <img src='./asset/png/gundamPoster.webp'> </div> <div><h2>Les personnages principaux de la série</h2> <div><p><b>Char Aznable</b></p></div></div>"
-        ]
-        [
-            "<p>Truc</p>"
+            "<div id='content__summary'> <h1>Mobile Suit Gundam</h1> <p>Année de sortie : 1979</p> <img src='./asset/png/gundamPoster.webp'> </div> <div id='content__characters'><h2>Les personnages principaux de la série</h2> <div><p><b>Char Aznable</b></p></div></div>"
         ]
     ]
 ];
@@ -40,7 +37,7 @@ butG1.addEventListener( "click", function ()
 
 function func_AnimeGundam ()
 {
-    animeContent.innerHTML = gundamAnimeTab[[animeContent_But]];
+    animeContent.innerHTML = gundamAnimeTab[animeContent_But];
 }
 
 function func_Desactiv() {
