2ac6e597cb06dedc452330c3888b9a6385f52699
StarkillerV22 <75523544+StarkillerV22@users.noreply.github.com>
2021-04-06 22:57:51 +0200

    G2 fini
    réréecriture des textes du 1er Gundam, refonte du dossier image et écriture des textes du 2eme Gundam.

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -13,11 +13,19 @@ function $$ ( expr, con )
 
 var animeContent = $( "#anime__content" );
 var butG1 = $( "#button__G1" );
+var butG2 = $( "#button__G2" );
+var butG3 = $( "#button__G3" );
 var G1 = $( "#G1" );
+var G2 = $( "#G2" );
+var G3 = $( "#G3" );
 var animeContent_But = 0;
+
+
 
 butG1.addEventListener( "click", function ()
 {
+    G2.style.display = "none";
+    G3.style.display = "none";
     if ( animeContent_But == 0 )
     {
         G1.style.display = "unset";
@@ -26,6 +34,38 @@ butG1.addEventListener( "click", function ()
     else
     {
         G1.style.display = "none";
+        animeContent_But = 0;
+    }
+} );
+
+butG2.addEventListener( "click", function ()
+{
+    G1.style.display = "none";
+    G3.style.display = "none";
+    if ( animeContent_But == 0 )
+    {
+        G2.style.display = "unset";
+        animeContent_But = 1;
+    }
+    else
+    {
+        G2.style.display = "none";
+        animeContent_But = 0;
+    }
+} );
+
+butG3.addEventListener( "click", function ()
+{
+    G1.style.display = "none";
+    G2.style.display = "none";
+    if ( animeContent_But == 0 )
+    {
+        G3.style.display = "unset";
+        animeContent_But = 1;
+    }
+    else
+    {
+        G3.style.display = "none";
         animeContent_But = 0;
     }
 } );
