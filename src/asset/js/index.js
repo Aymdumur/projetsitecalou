aeaaed1d8d0f7e1ffe17b7fdf32ffa8363001ddb
StarkillerV22 <75523544+StarkillerV22@users.noreply.github.com>
2021-04-06 10:56:15 +0200

    test JS 2

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -1,22 +1,33 @@
 // les helpers
-function $(expr, con) {
+function $ ( expr, con )
+{
     return typeof expr === "string" ?
-        (con || document).querySelector(expr) :
+        ( con || document ).querySelector( expr ) :
         expr;
 }
 
-function $$(expr, con) {
-    return Array.prototype.slice.call((con || document).querySelectorAll(expr));
+function $$ ( expr, con )
+{
+    return Array.prototype.slice.call( ( con || document ).querySelectorAll( expr ) );
 }
 
-var animeContent = $("#anime__content");
-var butG1 = $("#button__G1");
-var gundamAnimeTab = ["<p>Truc Bidule</p>"]
+var animeContent = $( "#anime__content" );
+var butG1 = $( "#button__G1" );
+var gundamAnimeTab = [
+    [
+        "<div>",
+            "<h1>Mobile Suit Gundam</h1>",
+            "<p>Année de sortie : 1979</p>",
+        "</div>"
+    ]
+];
 
-butG1.addEventListener("click", function(){
+butG1.addEventListener( "click", function ()
+{
     func_AnimeGundam();
-})
+} );
 
-function func_AnimeGundam() {
+function func_AnimeGundam ()
+{
     animeContent.innerHTML = gundamAnimeTab;
 }
